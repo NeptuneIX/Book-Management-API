650a583dea49e6f928053aaa0ba971d11a7c9d9e
NeptuneIX <=>
2024-10-22 23:33:12 +0200

    Changes

--- src/main/java/com/myprojects/bookmanagementapi/restfulwebservices/BookRepository.java
+++ src/main/java/com/myprojects/bookmanagementapi/restfulwebservices/BookRepository.java
@@ -2,6 +2,8 @@ package com.myprojects.bookmanagementapi.restfulwebservices;
 
 import com.myprojects.bookmanagementapi.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 }
--- src/main/java/com/myprojects/bookmanagementapi/restfulwebservices/BorrowerRepository.java
+++ src/main/java/com/myprojects/bookmanagementapi/restfulwebservices/BorrowerRepository.java
@@ -2,6 +2,8 @@ package com.myprojects.bookmanagementapi.restfulwebservices;
 
 import com.myprojects.bookmanagementapi.entities.Borrower;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BorrowerRepository extends JpaRepository<Borrower, Integer> {
 }
